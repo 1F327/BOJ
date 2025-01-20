b360f8ebcff2ae84c4c6780e3deba6574d5f0f46
Nagi🌧️ <wwcomms12@gmail.com>
2025-01-20 17:05:22 +0900

    Update 주사위 세개.py

--- Python/백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ Python/백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,10 +1,12 @@
-a, b, c = map(int, input().split())
-    
-if a == b == c:
-    print(10000 + a * 1000)
-elif a == b or a == c:
-    print(1000 + a * 100)
-elif b == c:
-    print(1000 + b * 100)
-else:
-    print(max(a, b, c) * 100)
+def calculate_prize(dice1, dice2, dice3):
+    if dice1 == dice2 == dice3:
+        print(10000 + dice1 * 1000)
+    elif dice1 == dice2 or dice1 == dice3:
+        print(1000 + dice1 * 100)
+    elif dice2 == dice3:
+        print(1000 + dice2 * 100)
+    else:
+        print(max(dice1, dice2, dice3) * 100)
+
+dice1, dice2, dice3 = map(int, input().split())
+calculate_prize(dice1, dice2, dice3)
