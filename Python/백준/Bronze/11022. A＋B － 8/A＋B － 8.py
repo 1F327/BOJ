eada66bff8402384d392c4bb70fbc1531c7e587b
Nagi🌧️ <wwcomms12@gmail.com>
2025-01-23 20:10:42 +0900

    [Bronze V] Title: A+B - 8, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -0,0 +1,7 @@
+def sum_2_numbers(case):
+    for i in range(case):
+        A, B = map(int, input().split())
+        print(f"Case #{i + 1}: {A} + {B} = {A + B}")
+            
+T = int(input())
+sum_2_numbers(T)
