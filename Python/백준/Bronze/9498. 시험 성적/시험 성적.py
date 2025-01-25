1d3dd51d189892992a798ecce83f34ffa3461317
Nagi🌧️ <wwcomms12@gmail.com>
2025-01-26 00:22:56 +0900

    Update 시험 성적.py

--- Python/백준/Bronze/9498. 시험 성적/시험 성적.py
+++ Python/백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -1,14 +1,14 @@
 def determine_grade(score):
-    if (90 <= score and score <= 100):
+    if 90 <= score <= 100:
         print("A")
-    elif (80 <= score and score <= 89):
+    elif 80 <= score <= 89:
         print("B")
-    elif (70 <= score and score <= 79):
+    elif 70 <= score <= 79:
         print("C")
-    elif (60 <= score and score <= 69):
+    elif 60 <= score <= 69:
         print("D")
     else:
         print("F")
-        
+
 score = int(input())
 determine_grade(score)
