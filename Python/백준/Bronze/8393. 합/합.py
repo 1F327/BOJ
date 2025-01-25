06483e7e07efb3ebf6d8cb2d65f6d3102547c32e
Nagi🌧️ <wwcomms12@gmail.com>
2025-01-26 00:49:33 +0900

    [Bronze V] Title: 합, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/8393. 합/합.py
+++ Python/백준/Bronze/8393. 합/합.py
@@ -1,8 +1,8 @@
-def sum_1_to_N(n):
+def sum_1_to_n(n):
     total_sum = 0
     for i in range(1, n + 1):
         total_sum += i
     print(total_sum)
 
 N = int(input())
-sum_1_to_N(N)
+sum_1_to_n(N)
