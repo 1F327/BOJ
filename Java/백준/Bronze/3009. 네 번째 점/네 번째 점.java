8be14c416a825562ff2fe88d3aee01489e572294
Renee🌧️ <wwcomms12@gmail.com>
2025-03-23 22:28:46 +0900

    [Bronze III] Title: 네 번째 점, Time: 184 ms, Memory: 18336 KB -BaekjoonHub

---
+++ Java/백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x1 = sc.nextInt(), y1 = sc.nextInt();
+		int x2 = sc.nextInt(), y2 = sc.nextInt();
+		int x3 = sc.nextInt(), y3 = sc.nextInt();
+
+		int x4 = x1 ^ x2 ^ x3;
+		int y4 = y1 ^ y2 ^ y3;
+
+		System.out.println(x4 + " " + y4);
+
+		sc.close();
+	}
+}
